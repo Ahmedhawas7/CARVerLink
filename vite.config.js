d6d1e3eff79aa443e294acdc915b86b0c2c49497
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-10-26 06:17:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/carverlink/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
