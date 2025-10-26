1c93e7c26483c6982c8ecb586fcd8648ec80c68a
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-10-26 06:34:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
 });
